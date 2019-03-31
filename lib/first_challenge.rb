6d5651c954f2e9017c9311de6bf4fdea020bbb17
Sarah Anderson <saranderson13@gmail.com>
2019-03-31 16:04:13 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -17,7 +17,7 @@ def first_challenge
 
   #your code here
   contacts["Freddy Mercury"].each do |attribute, value|
-    if attribute == :favorite_icecream_flavors
+    if attribute == :name
       confirmation = "You are here."
       binding.pry
     end
